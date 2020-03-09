31581deb276a4ea49ffa32af9fee456098ab5ae1
helian4096 <premjithsiddharth@gmail.com>
2020-03-09 15:58:31 -0400

    Lab06c Fixed runtime errors, created final page layout.

--- app/src/main/java/com/premjithsiddharth/quizappcustom/MainActivity.java
+++ app/src/main/java/com/premjithsiddharth/quizappcustom/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener{
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     EditText name;
@@ -28,8 +29,6 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
         name = findViewById(R.id.name);
         start = findViewById(R.id.start);
-        player = findViewById(R.id.name_container);
-        buttonCont = findViewById(R.id.button_continue);
         counter = 0;
         quiz = new String[][] {
                 {"Question 1. What is the capital of Russia?", "Berlin", "Kiev", "Moscow", "Ottawa"},
@@ -41,26 +40,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View v){
                 String str = "Player: " + name.getText();
-                player.setText(str);
                 setContentView(R.layout.main);
-                BlankFragment fragment = new BlankFragment();
+                player = findViewById(R.id.name_container);
+                player.setText(str);
+                buttonCont = findViewById(R.id.button_continue);
                 BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
+                BlankFragment fragment = new BlankFragment();
                 getSupportFragmentManager().beginTransaction().add(R.id.questions_fragment, fragment);
             }
         });
-        buttonCont.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                counter++;
-                if(counter >= 5){
-                    setContentView(R.layout.final_page);
-                }
-                else {
-                    BlankFragment fragment = new BlankFragment();
-                    BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
-                    getSupportFragmentManager().beginTransaction().add(R.id.questions_fragment, fragment);
-                }
+    }
+    public void nextPage(View v){
+        counter++;
+        if(counter >= 5){
+            setContentView(R.layout.final_page);
             }
-        });
+        else {
+            BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
+            BlankFragment fragment = new BlankFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.questions_fragment, fragment);
+            }
+        }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
