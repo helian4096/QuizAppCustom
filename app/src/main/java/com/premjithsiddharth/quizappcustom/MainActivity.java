266583c375ed0a9a6d7bc4ccbe266dae6a0bf2d2
helian4096 <premjithsiddharth@gmail.com>
2020-03-11 13:31:28 -0400

    Lab06c Fixed runtime errors, created final page layout, created replay button.

--- app/src/main/java/com/premjithsiddharth/quizappcustom/MainActivity.java
+++ app/src/main/java/com/premjithsiddharth/quizappcustom/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,12 +18,15 @@ public class MainActivity extends AppCompatActivity implements BlankFragment.OnF
     SharedPreferences.Editor editor;
     EditText name;
     Button start;
+    Button replay;
     TextView player;
     Button buttonCont;
     String[][] quiz;
     int counter;
+    LayoutInflater inflater;
+    ViewGroup group;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
         sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
@@ -30,30 +35,38 @@ public class MainActivity extends AppCompatActivity implements BlankFragment.OnF
         name = findViewById(R.id.name);
         start = findViewById(R.id.start);
         counter = 0;
+        inflater = getLayoutInflater();
         quiz = new String[][] {
                 {"Question 1. What is the capital of Russia?", "Berlin", "Kiev", "Moscow", "Ottawa"},
                 {"Question 2. What is the capital of Washington?", "Seattle", "Olympia", "Walla Walla", "Spokane"},
                 {"Question 3. What is the capital of Bulgaria?", "Sofia", "Belgrade", "Brussels", "Valletta"},
                 {"Question 4. What country has the capital city of Stockholm?", "Norway", "Finland", "Denmark", "Sweden"}
         };
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                String str = "Player: " + name.getText();
-                setContentView(R.layout.main);
-                player = findViewById(R.id.name_container);
-                player.setText(str);
-                buttonCont = findViewById(R.id.button_continue);
-                BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
-                BlankFragment fragment = new BlankFragment();
-                getSupportFragmentManager().beginTransaction().add(R.id.questions_fragment, fragment);
-            }
-        });
+    }
+    public void startGame (View v){
+        counter = 0;
+        String str = "Player: " + name.getText();
+        setContentView(R.layout.main);
+        player = findViewById(R.id.name_container);
+        player.setText(str);
+        buttonCont = findViewById(R.id.button_continue);
+        BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
+        BlankFragment fragment = new BlankFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.questions_fragment, fragment).commit();
     }
     public void nextPage(View v){
         counter++;
-        if(counter >= 5){
+        if(counter >= 4){
             setContentView(R.layout.final_page);
+            replay = findViewById(R.id.replay);
+            replay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick (View v){
+                    counter = 0;
+                    setContentView(R.layout.home_layout);
+
+                }
+            });
             }
         else {
             BlankFragment.newInstance(quiz[counter][0], quiz[counter][1], quiz[counter][2], quiz[counter][3], quiz[counter][4]);
